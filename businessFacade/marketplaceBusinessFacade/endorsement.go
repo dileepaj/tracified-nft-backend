62beaa0b8e043c26a9beae968c8aaefcee1f7f91
Keleigh123 <luciaberenger@gmail.com>
2022-06-20 17:18:41 +0530

    TRAC-5046-Created backend update endorsement for profile

--- businessFacade/marketplaceBusinessFacade/endorsement.go
+++ businessFacade/marketplaceBusinessFacade/endorsement.go
@@ -4,6 +4,7 @@ import (
 	"github.com/dileepaj/tracified-nft-backend/dtos/requestDtos"
 	"github.com/dileepaj/tracified-nft-backend/dtos/responseDtos"
 	"github.com/dileepaj/tracified-nft-backend/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func StoreEndorse(createEndorseObject models.Endorse) (string, error) {
@@ -24,4 +25,11 @@ func GetEndorsedStatus(publickey string) (models.Endorse, error) {
 
 func UpdateEndorsement(update requestDtos.UpdateEndorsementByPublicKey) (responseDtos.ResponseEndorsementUpdate, error) {
 	return EndorsementRepository.UpdateEndorsement(update)
+}
+
+func UpdateSetEndorsement(endorse requestDtos.UpdateEndorsement) (models.Endorse, error) {
+	update := bson.M{
+		"$set": bson.M{"name": endorse.Name, "email": endorse.Email, "contact": endorse.Contact, "desc": endorse.Description},
+	}
+	return EndorsementRepository.UpdateSetEndorsement("publickey", endorse.PublicKey, update)
 }
--- dtos/requestDtos/endorsement.go
+++ dtos/requestDtos/endorsement.go
@@ -4,3 +4,12 @@ type UpdateEndorsementByPublicKey struct {
 	PublicKey string `json:"publickey" bson:"publickey"`
 	Status    string `json:"status" bson:"status" `
 }
+
+type UpdateEndorsement struct {
+	PublicKey   string `json:"publickey" bson:"publickey"`
+	Status      string `json:"status" bson:"status" `
+	Name        string `json:"name" bson:"name" `
+	Email       string `json:"email" bson:"email" `
+	Contact     string `json:"contact" bson:"contact" `
+	Description string `json:"description" bson:"description" `
+}
