0835755ccef095efa1923b31cee55b9d6a9ca816
Mithila <mithilapanagoda@gmail.com>
2022-08-04 19:32:18 +0530

    TRAC-5598-fixed SVG formatting error

--- services/svgGeneratorforNFT/svgNFTGenerator/generator.go
+++ services/svgGeneratorforNFT/svgNFTGenerator/generator.go
@@ -26,7 +26,7 @@ func GenerateSVGTemplateforNFT(tdpData []models.TDP, batchID string) (string, er
 							 <div class="d-flex justify-content-center align-content-center flex-wrap" id="container">
 						`
 
-	var iframeImg = `<iframe  src="https://tracified.sirv.com/Spins/RURI%20Gems%20Compressed/120614/120614.spin" style="position: absolute; top: 0; left: 0; width: 100%; height: "300px" frameborder="0" allowfullscreen="true"></iframe>`
+	var iframeImg = `<iframe  src="https://tracified.sirv.com/Spins/RURI%20Gems%20Compressed/120614/120614.spin" style="top: 0; left: 0; width: 100%; height: 300px" frameborder="0" allowfullscreen="true"></iframe>`
 	for _, maindata := range tdpData {
 		numbersArr := make(map[string]string)
 		textArr := make(map[string]string)
