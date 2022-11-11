cc506e0af9889cc94cf3314c4db99265de7a3243
Mithila Panagoda <mithilapanagoda@gmail.com>
2022-11-11 11:59:35 +0530

    TRAC-6649 Added field currentownerpk to paginated response for NFTs

--- models/nft.go
+++ models/nft.go
@@ -35,14 +35,15 @@ type NFT struct {
 	Royalty           string             `json:"royalty" bson:"royalty"`
 }
 type NFTContentforMatrix struct {
-	NFTIdentifier string `json:"nftidentifier" bson:"nftidentifier" validate:"required"`
-	CreatorUserId string `json:"creatoruserid" bson:"creatoruserid" validate:"required"`
-	Blockchain    string `json:"blockchain" bson:"blockchain" validate:"required"`
-	NFTName       string `json:"nftname" bson:"nftname" validate:"required"`
-	Imagebase64   string `json:"imagebase64" bson:"imagebase64" validate:"required"`
-	SellingStatus string `json:"sellingstatus" bson:"sellingstatus" validate:"required"`
-	Trending      bool   `json:"trending" bson:"trending" `
-	HotPicks      bool   `json:"hotpicks" bson:"hotpicks" `
+	NFTIdentifier  string `json:"nftidentifier" bson:"nftidentifier" validate:"required"`
+	CreatorUserId  string `json:"creatoruserid" bson:"creatoruserid" validate:"required"`
+	Blockchain     string `json:"blockchain" bson:"blockchain" validate:"required"`
+	NFTName        string `json:"nftname" bson:"nftname" validate:"required"`
+	Imagebase64    string `json:"imagebase64" bson:"imagebase64" validate:"required"`
+	SellingStatus  string `json:"sellingstatus" bson:"sellingstatus" validate:"required"`
+	Trending       bool   `json:"trending" bson:"trending" `
+	HotPicks       bool   `json:"hotpicks" bson:"hotpicks" `
+	CurrentOwnerPK string `json:"currentownerpk" bson:"currentownerpk" validate:"required"`
 }
 
 type Paginateresponse struct {
