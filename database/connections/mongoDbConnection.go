22b5335181f10848f40d08519de9919dd1a54a54
Bhawantha Pasan Shanuka <bpsshanuka@gmail.com>
2023-01-09 12:00:33 +0530

    TRAC-6911 DB connection issue

--- database/connections/mongoDbConnection.go
+++ database/connections/mongoDbConnection.go
@@ -6,34 +6,32 @@ import (
 
 	"github.com/dileepaj/tracified-nft-backend/commons"
 	"github.com/dileepaj/tracified-nft-backend/utilities/logs"
-	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var DbName = commons.GoDotEnvVariable("DATABASE_NAME")
+var mgoSession mongo.Session
+var DbName=commons.GoDotEnvVariable("DATABASE_NAME")
 
 func GetMongoSession() (mongo.Session, error) {
+
 	connectionString := commons.GoDotEnvVariable("BE_MONGOLAB_URI")
-	var err error
-	mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connectionString))
-	if err != nil {
-		return nil, err
-	}
-	mgoSession, err := mongoClient.StartSession()
-	if err != nil {
-		log.Println("Error while connecting to the DB : " + err.Error())
-		return nil, err
-	}
-	defer func() {
-		if err = mongoClient.Disconnect(context.TODO()); err != nil {
-			logrus.Error("mongoClient Disconnect ", err)
+	if mgoSession == nil {
+		var err error
+		mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connectionString))
+		if err != nil {
+			return nil, err
 		}
-	}()
+		mgoSession, err = mongoClient.StartSession()
+		if err != nil {
+			log.Println("Error while connecting to the DB : " + err.Error())
+			return nil, err
+		}
+	}
 	return mgoSession, nil
 }
 
-func GetSessionClient(collection string) *mongo.Collection {
+func GetSessionClient(collection string) (*mongo.Collection){
 	session, err := GetMongoSession()
 	if err != nil {
 		log.Println("Error while getting session " + err.Error())
