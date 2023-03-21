be54b2065a20c1b3dd62b5363642ca718705c72f
Keleigh123 <luciaberenger@gmail.com>
2023-03-21 17:07:01 +0530

    TRAC-7918-Fixing TXN models in nftbackend

--- models/txn.go
+++ models/txn.go
@@ -5,10 +5,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type TXN struct {
 	Id            primitive.ObjectID `json:"Id" bson:"_id,omitempty"`
 	Blockchain    string             `json:"Blockchain" bson:"blockchain" `
-	Timestamp     primitive.DateTime `json:"Timestamp" bson:"timestamp,omitempty"`
 	NFTIdentifier string             `json:"NFTIdentifier" bson:"nftidentifier"`
 	Status        string             `json:"Status" bson:"status"`
 	NFTName       string             `json:"NFTName" bson:"nftname"`
 	ImageURL      string             `json:"ImageURL" bson:"imageurl"`
 	NFTTxnHash    string             `json:"NFTTxnHash" bson:"nfttxnhash"`
+	Time          string             `json:"Time" bson:"Time"`
 }
