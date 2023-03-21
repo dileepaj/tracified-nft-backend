91f5131dd002a4db8f1e6d1e73798bbb3266cad7
Mithila Panagoda <mithilapanagoda@gmail.com>
2023-03-21 12:43:21 +0530

    TRAC-7906-email-confirmation-issues-nft-marketplace-endorsement

--- businessFacade/marketplaceBusinessFacade/endorsement.go
+++ businessFacade/marketplaceBusinessFacade/endorsement.go
@@ -49,7 +49,7 @@ func SendEndorsmentEmail(endorsment models.Endorse) error {
 		msg := gomail.NewMessage()
 		msg.SetHeader("From", configs.GetEndrosmentSenderEmailAddres())
 		msg.SetHeader("To", endorsment.Email)
-		msg.SetHeader("Subject", "Tracified Marketplace Endorsment Response")
+		msg.SetHeader("Subject", "Tracified Marketplace Endorsement Response")
 		msg.SetBody("text/html", configs.GetAcceptedEndorsmentEmail(endorsment.Name, endorsment.Rating, endorsment.Review))
 		endorsmentEmail := gomail.NewDialer(
 			configs.GetEmailHost(),
@@ -57,7 +57,7 @@ func SendEndorsmentEmail(endorsment models.Endorse) error {
 			configs.GetEndrosmentSenderEmailAddres(),
 			configs.GetEndorsmentSenderEmailKey())
 		if err := endorsmentEmail.DialAndSend(msg); err != nil {
-			logs.ErrorLogger.Println("Failed to send Endrosment email: ", err.Error())
+			logs.ErrorLogger.Println("Failed to send Endorsment email: ", err.Error())
 			return err
 		}
 		logs.InfoLogger.Println("endorsment email sent to :", endorsment.Email)
@@ -66,7 +66,7 @@ func SendEndorsmentEmail(endorsment models.Endorse) error {
 		msg := gomail.NewMessage()
 		msg.SetHeader("From", configs.GetEndrosmentSenderEmailAddres())
 		msg.SetHeader("To", endorsment.Email)
-		msg.SetHeader("Subject", "Tracified Marketplace Endorsment Response")
+		msg.SetHeader("Subject", "Tracified Marketplace Endorsement Response")
 		msg.SetBody("text/html", configs.GetDeclinedEndorsmentEmail(endorsment.Name, endorsment.Rating, endorsment.Review))
 		endorsmentEmail := gomail.NewDialer(
 			configs.GetEmailHost(),
