850b71cc92214744c3cb507d7eea98355bda87c9
Bhawantha Pasan Shanuka <bpsshanuka@gmail.com>
2023-03-29 22:27:06 +0530

    TRAC-8023 fix indentation issue

--- models/nft.go
+++ models/nft.go
@@ -6,8 +6,8 @@ import (
 
 type NFT struct {
 	Id                primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	NFTIdentifier     string             `json:"nftidentifier" bson:"nftidentifier" validate:"required"` //polygon eth ==> "tokenId - contractAddress ,stellar==> assetName - NFT issuer PK
-	CreatorUserId     string             `json:"creatoruserid" bson:"creatoruserid" validate:"required"` //minter user id //stellar==> distributor user Id assect creator user id
+	NFTIdentifier     string             `json:"nftidentifier" bson:"nftidentifier" validate:"required"` // polygon eth ==> "tokenId - contractAddress ,stellar==> assetName - NFT issuer PK
+	CreatorUserId     string             `json:"creatoruserid" bson:"creatoruserid" validate:"required"` // minter user id //stellar==> distributor user Id assect creator user id
 	Blockchain        string             `json:"blockchain" bson:"blockchain" validate:"required"`
 	NFTName           string             `json:"nftname" bson:"nftname" validate:"required"`
 	NftContentURL     string             `json:"nftcontenturl" bson:"nftcontenturl"`
@@ -20,12 +20,12 @@ type NFT struct {
 	AttachmentType    string             `json:"attachmenttype" bson:"attachmenttype" validate:"required"`
 	CurrentPrice      string             `json:"currentprice" bson:"currentprice" `
 	CurrentOwnerPK    string             `json:"currentownerpk" bson:"currentownerpk" validate:"required"`
-	IssuerPK          string             `json:"nftissuerpk" bson:"nftissuerpk" validate:"required"` //minter pK for POLYGON.ETH.Solana , stellar ==>unioque created account
-	ArtistName        string             `json:"artistname" bson:"artistname"`                       //spacific artisct
+	IssuerPK          string             `json:"nftissuerpk" bson:"nftissuerpk" validate:"required"` // minter pK for POLYGON.ETH.Solana , stellar ==>unioque created account
+	ArtistName        string             `json:"artistname" bson:"artistname"`                       // spacific artisct
 	ArtistProfileLink string             `json:"artistprofilelink" bson:"artistprofilelink"`
-	SellingStatus     string             `json:"sellingstatus" bson:"sellingstatus" validate:"required"` //ONSALE,NOTSALE,NOTLISTED
-	SellingType       string             `json:"sellingtype" bson:"sellingtype" `                        //NOTLISTED
-	DistributorPK     string             `json:"distributorpk" bson:"distributorpk"`                     //specific for stellar (loged user PK when start to mint)
+	SellingStatus     string             `json:"sellingstatus" bson:"sellingstatus" validate:"required"` // ONSALE,NOTSALE,NOTLISTED
+	SellingType       string             `json:"sellingtype" bson:"sellingtype" `                        // NOTLISTED
+	DistributorPK     string             `json:"distributorpk" bson:"distributorpk"`                     // specific for stellar (loged user PK when start to mint)
 	MraketContract    string             `json:"marketcontract" bson:"marketcontract"`
 	MintedContract    string             `json:"mintcontract" bson:"mintcontract"`
 	TokenType         string             `json:"tokentype" bson:"tokentype"`
@@ -75,7 +75,6 @@ type PaginatedCreatorInfo struct {
 	PaginationInfo PaginationTemplate
 }
 
-
 type ContractInfo struct {
 	ContractAddress string `json:"contractaddress" bson:"contractaddress"`
 	User            string `json:"user" bson:"user"`
@@ -95,7 +94,7 @@ type WalletNFT struct {
 	Email           string `json:"email" bson:"email"`
 	Timestamp       string `json:"timestamp" bson:"timestamp"`
 	TXNHash         string `json:"txnhash" bson:"txnhash"`
-	ShopID 			string `json:"shopid" bson:"shopid"`
+	ShopID          string `json:"shopid" bson:"shopid"`
 }
 
 type ResponseWalletNFT struct {
@@ -110,5 +109,5 @@ type ResponseWalletNFT struct {
 	Email           string `json:"email" bson:"email"`
 	Timestamp       string `json:"timestamp" bson:"timestamp"`
 	TXNHash         string `json:"txnhash" bson:"txnhash"`
-	ShopID 			string `json:"shopid" bson:"shopid"`
+	ShopID          string `json:"shopid" bson:"shopid"`
 }
